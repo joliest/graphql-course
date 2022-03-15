31d60ebfd5af80dbd42297784641884e12118950
Joliver Estampador <joliverestampador@gmail.com>
2022-03-16 06:51:36 +0800

    scalar types

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,11 +2,31 @@ import { GraphQLServer } from 'graphql-yoga';
 
 const typeDefs = `
     type Query {
+        id: ID!
+        name: String!
+        age: Int!
+        employed: Boolean!
+        gpa: Float
     }
 `
 
 const resolvers = {
     Query: {
+        id() {
+            return 'abc123'
+        },
+        name() {
+            return 'Joli'
+        },
+        age() {
+            return '29'
+        },
+        employed() {
+            return true
+        },
+        gpa() {
+            return null
+        },
     }
 }
 
