9718d76cc7891bcc95731f3ca50143c3f1c60f16
Joliver Estampador <joliverestampador@gmail.com>
2022-03-16 06:48:00 +0800

    cleanup

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,31 +1,12 @@
 import { GraphQLServer } from 'graphql-yoga';
 
-// Type definitions - a.k.a application schema
 const typeDefs = `
     type Query {
-        hello: String!
-        name: String!
-        location: String!
-        bio: String!
     }
 `
 
-// resolvers for api
 const resolvers = {
     Query: {
-        // one method for each query we setup
-        hello() {
-            return 'This is my first query!'
-        },
-        name() {
-            return 'Andrew Mead'
-        }, 
-        location() {
-            return 'Apalit'
-        }, 
-        bio() {
-            return 'I am a software developer'
-        }
     }
 }
 
