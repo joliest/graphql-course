84ec9dcece123835ddeeb53a5a55dc44dfdf64b4
Joliver Estampador <joliverestampador@gmail.com>
2022-03-16 06:40:18 +0800

    creating a schema

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,39 @@
-console.log('Hello graphql')
+import { GraphQLServer } from 'graphql-yoga';
+
+// Type definitions - a.k.a application schema
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`
+
+// resolvers for api
+const resolvers = {
+    Query: {
+        // one method for each query we setup
+        hello() {
+            return 'This is my first query!'
+        },
+        name() {
+            return 'Andrew Mead'
+        }, 
+        location() {
+            return 'Apalit'
+        }, 
+        bio() {
+            return 'I am a software developer'
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+})
+
+server.start(() => {
+    console.log('The server is app');
+});
