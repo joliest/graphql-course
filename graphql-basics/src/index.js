9e20fc98481eeeee42bb544d36ce26f033426a10
Joliver Estampador <joliverestampador@gmail.com>
2022-03-21 07:55:47 +0800

    creating custom types

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,31 +2,43 @@ import { GraphQLServer } from 'graphql-yoga';
 
 const typeDefs = `
     type Query {
+        me: User!
+        post: Post!
+    }
+    
+    type User {
         id: ID!
         name: String!
-        age: Int!
-        employed: Boolean!
-        gpa: Float
+        email: String!
+        age: Int
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
     }
 `
 
 const resolvers = {
     Query: {
-        id() {
-            return 'abc123'
+        me() {
+            return {
+                id: '123098',
+                name: 'Mike',
+                email: 'mike@example.com',
+                age: 28,
+            }
         },
-        name() {
-            return 'Joli'
-        },
-        age() {
-            return '29'
-        },
-        employed() {
-            return true
-        },
-        gpa() {
-            return null
-        },
+        post() {
+            return {
+                id: '123097',
+                title: 'My Post',
+                body: 'Lorem ipsum chena',
+                published: false,
+            }
+        }
     }
 }
 
