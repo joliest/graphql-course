590b37aaba0ca31e71d3291ed97c315dd3df9c9f
Joliver Estampador <joliverestampador@gmail.com>
2022-05-14 07:21:24 +0800

    clean up

--- graphql-basics/src/resolvers/Subscription.js
+++ graphql-basics/src/resolvers/Subscription.js
@@ -1,25 +1,4 @@
 const Subscription = {
-    count: {
-        /** what runs when something subscribes */
-        subscribe(parent, args, { pubsub }, info) {
-            // setup subscription
-            let count = 0;
-
-            setInterval(() => {
-                count++
-                // publishes the data
-                // trigger name should match with a channel name
-                pubsub.publish('count', {
-                    count,
-                })
-
-            }, 1000)
-
-            // assign a channel name (like a chat name)
-            // asyncIterator sets the channel
-            return pubsub.asyncIterator('count');
-        }
-    },
     comment: {
         subscribe(parent, { postId }, { pubsub, db }, info) {
             const post = db.posts.find((post) => post.id === postId && post.published);
