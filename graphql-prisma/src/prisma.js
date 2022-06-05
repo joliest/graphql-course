4b26de7a60ea1b78a9aa1f460cba86fac469a411
Joliver Estampador <joliverestampador@gmail.com>
2022-06-06 07:31:09 +0800

    clean up

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -1,9 +1,6 @@
 import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
-    // this will be auto generated, configured in .graphqlconfig
     typeDefs: 'src/generated/prisma.graphql',
-    // docker's localhost and port
     endpoint: 'localhost:4466',
-
 })
