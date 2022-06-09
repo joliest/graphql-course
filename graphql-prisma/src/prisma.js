7bb44dce6670be03c0c9168edef5e6900202f536
Joliver Estampador <joliverestampador@gmail.com>
2022-06-10 07:18:06 +0800

    Check if posts exists

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -26,8 +26,12 @@ const post = `
         published
     }
 `
-
 const createPostForUser = async (authorId, data) => {
+    const userExists = await prisma.exists.User({ id: authorId });
+    if (!userExists) {
+        throw new Error('User not found')
+    }
+
     const post = await prisma.mutation.createPost({
         data: {
             ...data,
@@ -37,38 +41,44 @@ const createPostForUser = async (authorId, data) => {
                 }
             }
         },
-    }, '{ id }')
-    const user = await prisma.query.user({
-        where: {
-            id: authorId
-        }
-    }, userQuery)
-    return user
+    }, '{ author { id name email posts { id title published} } }')
+    return post.author
 }
 
 // createPostForUser('cl3p9abv900380f284da2yfuv', {
-//     title: 'Great books to read 2',
-//     body: 'The war of art 2',
+//     title: 'Great books to read 4',
+//     body: 'The war of art 4',
 //     published: true,
 // }).then((user) => {
 //     console.log(JSON.stringify(user, undefined, 2))
+// }).catch(error => {
+//     console.log(error)
 // })
 
 const updatePostForUser = async (postId, data) => {
+    const postExists = await prisma.exists.Post({
+        id: postId,
+    });
+    if (!postExists) {
+        throw new Error('Post not found')
+    }
     const newPost = await prisma.mutation.updatePost({
         data,
         where: {
             id: postId
         },
-    }, post)
+    }, '{ author { id name email posts { id title published } } }')
     return newPost;
 }
 
-updatePostForUser('cl41yuetc003i0g28zyx6t1cn', { published: false })
+updatePostForUser('cl41yuetc00s3i0g28zyx6t1cn', { published: false })
     .then((post) => {
         console.log(JSON.stringify(post, undefined, 2))
     })
-/*
+    .catch((error) => {
+        console.log(error)
+    })
+
 prisma.mutation.createPost({
     data: {
         title: 'lorem ipsum',
@@ -86,7 +96,7 @@ prisma.mutation.createPost({
 }).then((data) => {
     console.log(JSON.stringify(data, undefined, 2))
 })
- */
+
 
 /*
 prisma.mutation.updatePost({
